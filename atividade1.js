8989fccddaa20ee83b6ca482fcdf3f506c23c03d
DanielaRodrigueS2 <danielarodrigues2po@gmail.com>
2024-03-15 15:11:16 -0300

    Update atividade1.js

--- atividade1.js
+++ atividade1.js
@@ -19,4 +19,39 @@ function retornaString(n, msg) {
     return mensagem;
 }
 
-// Exercício 3:
+// Exercício 3: 0 - Adição, 1 - Subtração, 2 - Multiplicação, 3 - Divisão
+
+function calc(num1, num2, op) {
+    switch (op) {
+        case 0:
+            return num1 + num2;
+            break;
+        case 1:
+            return num1 - num2;
+            break;
+        case 2:
+            return num1 * num2;
+            break;
+        case 3:
+            if (num2 != 0) {
+                return num1 / num2;
+            }
+            else console.log("Divisão inválida");
+            return 0;
+        default:
+            console.log("Opção inválida");
+            return 0;
+            break;
+    }
+}
+// Exercício 4:
+
+function tabuada(num) {
+    let vetor = new Array();
+    for (i = 0; i <= 10; i++) {
+        vetor[i] = num * i;
+    }
+    return vetor;
+}
+
+// Exercício 5:
