61e9812c4c87ab58c2f7166184db3bff04cef5d9
DanielaRodrigueS2 <danielarodrigues2po@gmail.com>
2024-03-15 12:50:12 -0300

    Exercicio 1
    Calculo de fatorial

---
+++ atividade1.js
@@ -0,0 +1,13 @@
+// Exercício 1:
+
+function fatorial(num) {
+    if (num == 0) return 1;
+    const resultado = 1;
+    for (i = num; i > 0; i--) {
+        resultado = resultado * i;
+    }
+    return resultado;
+};
+
+// Exercício 2:
+
